216f590b2d79933020529aae854e8aaaa1bf83c4
JimmyHou <hqsh200459@outlook.com>
2025-10-04 15:04:57 +0800

    Finish testing CategoryForm

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,7 +20,8 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js",
+  testMatch: [
+  //"<rootDir>/client/src/pages/Auth/*.test.js",
   "<rootDir>/client/src/pages/admin/AdminOrders.test.js",
   "<rootDir>/client/src/pages/Policy.test.js",
   "<rootDir>/client/src/components/Footer.test.js",
@@ -28,12 +29,14 @@ export default {
   "<rootDir>/client/src/components/Layout.test.js",
   "<rootDir>/client/src/components/Spinner.test.js",
   "<rootDir>/client/src/pages/About.test.js",
-  "<rootDir>/client/src/pages/Pagenotfound.test.js"
+  "<rootDir>/client/src/pages/Pagenotfound.test.js",
+  "<rootDir>/client/src/components/Form/CategoryForm.test.js"
   ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**",
+  collectCoverageFrom: [
+  //"client/src/pages/Auth/**",
   "client/src/pages/admin/AdminOrders.js",
   "client/src/pages/Policy.js",
   "client/src/components/Footer.js",
@@ -41,7 +44,8 @@ export default {
   "client/src/components/Layout.js",
   "client/src/components/Spinner.js",
   "client/src/pages/About.js",
-  "client/src/pages/Pagenotfound.js"
+  "client/src/pages/Pagenotfound.js",
+  "client/src/components/Form/CategoryForm.js"
   ],
 
 
